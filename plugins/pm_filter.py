d932c819fe7aa1fc2245345b14f6c09780afb1dd
Thirank <116568300+Thirank@users.noreply.github.com>
2023-06-08 11:03:30 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -397,23 +397,15 @@ async def language_check(bot, query):
         )
 
         try:
-
             await query.edit_message_reply_markup(
-
                 reply_markup=InlineKeyboardMarkup(btn)
-
             )
-
         except MessageNotModified:
-
             pass
-
         await query.answer()
-
     else:
-
         return await query.answer(f"Sᴏʀʀʏ, Nᴏ ғɪʟᴇs ғᴏᴜɴᴅ ғᴏʀ ʏᴏᴜʀ ᴏ̨ᴜᴇʀʏ {movie}.", show_alert=True)
-
+    
 @Client.on_callback_query(filters.regex(r"^select_lang"))
 
 async def select_language(bot, query):
